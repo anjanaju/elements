e897a443de69d4f9aae55cdb2db93ae173dbd041
Anjana Das <anjanaju@gmail.com>
2015-04-17 00:19:57 +0530

    Calculate the number of 1s in a big number

--- ch4/parity.py
+++ ch4/parity.py
@@ -5,8 +5,27 @@ def Parity(x):
     while x>0:
         x = x&(x-1)
         count = count+1
-    return count%2
+    return count
+
+def initDict():
+    d = dict()
+    for i in range(0, 16):
+        j= Parity(i)
+        d[i] = j
+    return d
+
+def Calculate(x,d):
+    count = 0
+    for i in range(1,9):
+        y = x & 15
+        count = count + d[y]
+        x = x >> 4
+    return count
+
+d = initDict()
+for i in range(0, 16):
+    print(i, d[i])
+j = Calculate(31, d)
+print(j)
 
 
-y = Parity(11)
-print(y)
